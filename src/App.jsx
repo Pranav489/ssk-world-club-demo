9555386800a8ce85d4a27489517db6965321acc8
Pranav489 <pranavsonawane489@gmail.com>
2025-08-12 14:45:06 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,20 @@ import {
 import Layout from './Components/Layout/Layout'
 import Home from './Pages/Home/Home'
 import PageNotFound from './Pages/ErrorPages/NotFound'
-import AboutUs from './Pages/AboutUs/AboutUs'
+// import AboutUs from './Pages/AboutUs/AboutUs'
 import SSKSolitaireGuestForm from './Pages/SSKSolitaireGuestForm/SSKSolitaireGuestForm'
 import WalkingVisitorForm from './Pages/SSKWalkingVisitorForm/WalkingVisitorForm'
-import Sports from './Pages/Sports/Sports'
 import SportsDetailPage from './Pages/Sports/SportsDetailsPage'
+import AboutUsPage from './Pages/AboutUs/AboutUsPage'
+import SportsPage from './Pages/Sports/SportsPage'
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path='about-us' element={<AboutUs />} />
-        <Route path="/sports" element={<Sports />} />
+        <Route path='about-us' element={<AboutUsPage />} />
+        <Route path="/sports" element={<SportsPage />} />
         <Route path="/sports/:category/:sportSlug" element={<SportsDetailPage />} />
         <Route path='ssk-solitaire-guest-form' element={<SSKSolitaireGuestForm />} />
         <Route path='ssk-walking-visitor-form' element={<WalkingVisitorForm />} />
